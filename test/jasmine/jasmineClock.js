9c5fdaa932681f84cbf27835bc0c710950e5c73e
Wei Kin Huang <weikin.huang04@gmail.com>
2016-09-26 10:54:49 -0400

    Add test for src/jasmineClock

--- test/jasmine/jasmineClock.js
+++ test/jasmine/jasmineClock.js
@@ -1,7 +1,39 @@
 import { overrideJasmineClock } from '../../src/jasmineClock';
+import { realSetTimeout } from '../../src/timers';
 
 describe('jasmineClock', function() {
-  it('#overrideJasmineClock', function() {
-    expect(1).toEqual(1);
+  beforeAll(function() {
+    // jasmine clock relies on having access to the original timer functions
+    jasmine.clock = overrideJasmineClock(jasmine.clock);
+  });
+
+  beforeEach(function() {
+    jasmine.clock().install();
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+  });
+
+  it('#overrideJasmineClock should work as the without throws', function(done) {
+    const spy = jasmine.createSpy('timeout callback');
+    setTimeout(spy, 15);
+
+    expect(spy).not.toHaveBeenCalled();
+    jasmine.clock().tick(16);
+    expect(spy).toHaveBeenCalled();
+
+    // to make sure we don't throw in the test
+    realSetTimeout(done, 25);
+  });
+
+  it('#overrideJasmineClock should work as the without throws', function(done) {
+    const spy = jasmine.createSpy('timeout callback');
+    setTimeout(spy, 25);
+
+    expect(spy).not.toHaveBeenCalled();
+
+    // to make sure we don't throw in the test
+    realSetTimeout(done, 1);
   });
 });
